cd2442244b75fec7ae5b513b72151ffd998d6c11
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-26 13:58:03 +0300

    Sample Quote Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockformComponent } from './blockform/blockform.component';
 
- 
-
+//
+ import { FormsModule }from '@angular/forms';
+//
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { BlockformComponent } from './blockform/blockform.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/blockform/blockform.component.ts
+++ src/app/blockform/blockform.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blockform',
   templateUrl: './blockform.component.html',
-  styleUrls: ['./blockform.component.css']
+  styleUrls: ['./blockform.component.css'],
+  
 })
 export class BlockformComponent implements OnInit {
-  
-  quotedetails = 'quoteinfo';
-
-  quoteinfo = {
-    name : "Alex",
-    age: "new",
-  };
-
-
-  
-  constructor() { }
-
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  
+  submit(){
+    console.log("Form submitted",)
   }
 
-}
+  samples = [
+    { quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", name: "John", author:"Nelson Mandela"},
+    { quote: "The way to get started is to quit talking and begin doing.", name: "alex1", author:"Walt Disney"},
+    { quote: "If life were predictable it would cease to be life, and be without flavor.", name: "alex2", author:"Eleanor Roosevelt"}
+  ];
 
+  
+
+ 
+
+
+
+
+
+
+
+}
